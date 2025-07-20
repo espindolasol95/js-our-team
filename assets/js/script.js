b1a4489716e9b774018f0930155bcc0f3191c8cd
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-20 22:26:04 +0200

    feat(coppio incollo html)

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,12 +38,36 @@ const teamMembers = [
 ];
 // recuperare gli elemneti del dom che mi servono 
 
-const teamContainer =
+const teamContainer =document.querySelector(".teamContainer .row");
+
 //definisco una variabile che mi servira per contenere tutte le colonne e le card da innietare nella pagina
 let cards ='';
 //ciclo larray 
 
-for(let i=0; i<teamMembers.length; i++);
+for(let i=0; i<teamMembers.length; i++){
 const member = teamMembers [i];
 const {name, role, email, img} = member;
-cards+=
+cards+= `
+    <div class="col-md-6 col-lg-4 mb-4">
+      <div class="card mb-3 bg-dark">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${img}" class="img-fluid rounded-start" alt="${name}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body text-white">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+              <p class="card-text"><small>${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;}
+
+  // Inietta le card nella pagina
+  teamContainer.innerHTML = cards;
+  //bonus:
+
+  
